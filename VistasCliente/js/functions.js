30cc25a863d26038cba5935a715608a19e25dab8
Allan <amarinl301@ulacit.ed.cr>
2019-08-15 02:20:03 -0600

    Added validations to user creation

--- VistasCliente/js/functions.js
+++ VistasCliente/js/functions.js
@@ -9,7 +9,7 @@ APP.functions = (function() {
         create: 'Create',
         update: 'Update',
         delete: 'Delete'
-    }
+    };
 
     var makeAPICall = function(apiName, apiCall, method, params, onSuccess, onError) {
         $.ajax({
@@ -27,11 +27,11 @@ APP.functions = (function() {
                 }
             }
         });
-    } 
+    };
 
     var bindButtons = function() {
         $('input:checkbox').each(function() {
-            var $action = $(this).attr('data-action');
+            var $action = $(this).attr('data-function');
             if ($action === 'toggleEnable') {
                 $(this).on('click', function() {
                     resetEnabled(this);
@@ -41,7 +41,17 @@ APP.functions = (function() {
 		$('.reset-form').on('click', function(e) {
 			e.preventDefault();
 			resetForm(this);
-		});
+        });
+        $('form[data-function="initialize"]').each(function() {
+            initializeForm(this);
+        })
+        $('form').on('submit', function (e, options) {
+            options = options || {};
+            if (!options.validated) {
+                e.preventDefault();
+                validateFormAndSubmit(this, e);
+            }
+        });
     };
 
 	var resetForm = function(button) {
@@ -53,20 +63,71 @@ APP.functions = (function() {
             });
         }
     };
+
+    var initializeForm = function (form) {
+        var $form = $(form);
+        $form.find('input:checkbox').each(function () {
+            resetEnabled(this);
+        });
+        $form.find('[data-function="noBlank"]').each(function() {
+            var $label = $($(this).closest('.form-group').find('.control-label')[0]);
+            if ($label) {
+                $label.html($label.html() + ' <span class="text-danger">*</span>');
+            }
+        });
+        $form.find('[data-function="noBlank"]').each(function () {
+            $(this).on('blur', function () {
+                validateNoBlank(this);
+            });
+        });
+    };
+
+    var validateForm = function (form, e) {
+        var $form = $(form);
+        var $valid = true;
+        $form.find('[data-function="noBlank"]').each(function () {
+            $valid = validateNoBlank(this) && $valid;
+        });
+        if ($valid) {
+            $(e.currentTarget).trigger(e.type, { 'validated': true });
+        }
+    };
+
+    var validateFormAndSubmit = function (form, e) {
+        var $valid = validateForm(form);
+        if ($valid) {
+            $(e.currentTarget).trigger(e.type, { 'validated': true });
+        }
+    };
+
+    var validateNoBlank = function(field) {
+        var $field = $(field);
+        var $valid = true;
+        var $message = $($field.siblings('.text-danger')[0]);
+        $message.html('');
+
+        if (!$field.attr('disabled')) {
+            if (!($field.val() || $field.val().trim())) {
+                $message.html('Este campo es requerido');
+                $valid = false;
+            }
+        }
+        else {
+            $field.val('');
+        }
+        return $valid;
+    };
     
     var resetEnabled = function(checkbox) {
         var $target = $(checkbox).attr('data-target');
-        var $action = $(checkbox).attr('data-action');
+        var $action = $(checkbox).attr('data-function');
         if ($action === 'toggleEnable') {
             $($target).each(function() {
                 $(this).attr('disabled', !checkbox.checked);
+                validateNoBlank(this);
             });
         }
-    }
-
-    var getAPIKey = function(key) {
-        return API_KEYS[key];
-    }
+    };
 
     var init = function() {
         bindButtons();
@@ -74,10 +135,11 @@ APP.functions = (function() {
 
     return {
         init: init,
-        makeAPICall: makeAPICall
-    }
+        makeAPICall: makeAPICall,
+        validateForm: validateForm
+    };
 }());
 
-window.onload = function() {
+$(window).on('load', function() {
     APP.functions.init();
-}
+});
