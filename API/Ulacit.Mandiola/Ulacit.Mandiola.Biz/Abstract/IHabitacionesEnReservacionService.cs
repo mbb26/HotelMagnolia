d08d4fdee0c0a125ca601f63c5fcd34e8dc04d71
Twhite1195 <twhite1195@gmail.com>
2019-08-29 22:38:20 -0600

    cherry pick habitaciones en reservacion service

---
+++ API/Ulacit.Mandiola/Ulacit.Mandiola.Biz/Abstract/IHabitacionesEnReservacionService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Ulacit.Mandiola.Common.Abstract;
+
+namespace Ulacit.Mandiola.Biz.Abstract
+{
+    /// <summary>Interface for Habitaciones in reservationservice.</summary>
+    public interface IHabitacionesEnReservacionService : IBasicService
+    {
+        List<T> GetJoinHabsEnResv<T>(string IDReservacion);
+    }
+}
